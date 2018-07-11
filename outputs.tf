c52fa8bccc162a1df92deff1e33b094235406040
Koen de Laat <koen.de.laat@philips.com>
2018-07-11 19:13:07 +0200

    Addedextra outputs

--- outputs.tf
+++ outputs.tf
@@ -12,3 +12,13 @@ output "service_role_name" {
   description = "Created IAM service role name."
   value       = "${aws_iam_role.ecs_service.name}"
 }
+
+output "iam_instance_profile_name" {
+  description = ""
+  value       = "${aws_iam_instance_profile.ecs_instance.name}"
+}
+
+output "instance_sg_id" {
+  description = ""
+  value       = "${aws_security_group.instance_sg.id}"
+}
